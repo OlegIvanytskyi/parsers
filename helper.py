f389b3f21168fa035bf9eafc15f50917d2663761
Oleg Ivanytskyi <oleg.ivanytskyi@gmail.com>
2020-06-17 01:03:36 +0300

    delete old data and write new parser

--- helper.py
+++ helper.py
@@ -14,12 +14,11 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 def write_to_file(arr, category, website):
-    num_of_articles = 0
     path = f'data/{category}/{website}/'
     pathlib.Path(path).mkdir(parents=True, exist_ok=True)
     
     with codecs.open(path + 'new.txt', 'w', 'utf8') as f:
-        for elem in arr:
+        for elem in arr:  # elem - contains teaser_headline and teaser itself
             f.write((elem[0] + '\n' + elem[1] + '\n'))
-            num_of_articles += 1
-    print(f'{num_of_articles} articles of {category} category scraped from {website}\n')
+            
+    print(f'{len(arr)} articles of {category} category have been scraped from {website}\n')
